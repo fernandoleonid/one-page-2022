99ec1103fbb08995a44f82b0d2ac77b87479cbe7
EDUARDO RIBEIRO PIMENTA <22282115@CFP127ALUNO.LOCAL>
2022-11-29 10:09:52 -0300

    js do carrousel pronto

--- ds1m-b/Eduardo Ribeiro Pimenta/js/carrousel.js
+++ ds1m-b/Eduardo Ribeiro Pimenta/js/carrousel.js
@@ -3,19 +3,48 @@ const prev = document.getElementById('prev');
 const images = document.querySelector('.images-container');
 
 let current_card = 0;
-
+let primeiro_card = 260;
+let contador = 0;
 next.addEventListener('click', () => {
-    if(current_card >1){
-        current_card++;
-        images.style.transitionDuration = "0.5s";
-        images.style.transform = "translate(-"+ current_card * 500 +"px)";
+
+    if(current_card < images.children.length -1 ){
+        if(contador == 0){
+            contador++;
+            images.style.transitionDuration = "0.5s";
+            images.style.transform = "translate(-"+ (primeiro_card) +"px)";
+            
+        }else{
+            current_card++;
+            contador++;
+            images.style.transitionDuration = "0.5s";
+            images.style.transform = "translate(-"+ (primeiro_card + 520 * current_card) +"px)";
+        }
+
+        
     }else{
-        current_card++;
-        images.style.transitionDuration = "0.5s";
-        images.style.transform = "translate(-"+ current_card * 500 +"px)";
+        return;
     }
 
 
 });
+
+prev.addEventListener('click', () => {
+    if(contador == 1){
+        contador--;
+        images.style.transitionDuration = "0.5s";
+        images.style.transform = "translate(0px)";
+    }
+    if(current_card > 0){
+        current_card--;
+        contador--;
+        images.style.transitionDuration = "0.5s";
+        images.style.transform = "translate(-"+ (520 * current_card + primeiro_card) +"px)";
+
+    }
+
+
+
+});
+
 
 
